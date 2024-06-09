5d35ef36797e165892416bab0cbf20c3d2646b95
JuliaArbuzova <154761916+JuliaArbuzova@users.noreply.github.com>
2024-06-10 01:37:25 +0300

    Delete microservices/microservices directory

--- microservices/microservices/orderservice/src/main/kotlin/com/example/order/rep/StationRep.kt
+++
@@ -1,8 +0,0 @@
-package com.example.order.repository
-
-import com.example.order.model.Station
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface StationRep : JpaRepository<Station, Long>
--- microservices/microservices/settings.gradle.kts
+++
@@ -1,2 +0,0 @@
-rootProject.name = "ticketing-system"
-include("authservice", "orderservice")
